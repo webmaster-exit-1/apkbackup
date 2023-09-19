5f10f8d708d391931a86549ff796725706ad1b57
webmaster-exit-1 <mrgofuckyourself@pm.me>
2023-09-19 08:25:21 -0700

    Create test_apkbackup.py

---
+++ tests/test_apkbackup.py
@@ -0,0 +1,26 @@
+# tests/test_apkbackup.py
+
+import unittest
+from unittest.mock import patch
+import sys
+import os
+
+# Adjust the sys.path to include the root directory of your project.
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from apkbackup import YourMainModuleFunction  # Replace this with your actual import
+
+class TestAPKBackup(unittest.TestCase):
+
+    @patch('subprocess.run')
+    def test_some_functionality(self, mock_run):
+        mock_run.return_value = "Expected adb output"
+
+        # Call your function that interacts with ADB.
+        result = YourMainModuleFunction()
+
+        # Assert the expected behavior/output.
+        self.assertEqual(result, "Expected output based on mock")
+
+if __name__ == "__main__":
+    unittest.main()

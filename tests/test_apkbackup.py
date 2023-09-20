f5d0788522a8ce9cf4bebc5b61db50b24d16436e
webmaster-exit-1 <mrgofuckyourself@pm.me>
2023-09-20 04:05:21 -0700

    Update test_apkbackup.py

--- tests/test_apkbackup.py
+++ tests/test_apkbackup.py
@@ -4,11 +4,12 @@ import unittest
 from unittest.mock import patch
 import sys
 import os
+import subprocess  # Ensure this import is present
+
+from apkbackup import backup_apk  # Only import what you're using
 
 # Adjust the sys.path to include the root directory of your project.
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from apkbackup import get_installed_packages, backup_apk
 
 class TestAPKBackup(unittest.TestCase):
 
@@ -18,7 +19,7 @@ class TestAPKBackup(unittest.TestCase):
         mock_run.return_value = subprocess.CompletedProcess(args=[], returncode=0, stdout="Expected adb output", stderr="")
 
         # Call your function that interacts with ADB.
-        backup_apk('mock_package_name')
+        backup_apk(mock_package_name)
 
         # Assert the expected behavior/output.
         # For example, check if the mock_run was called with the expected arguments.

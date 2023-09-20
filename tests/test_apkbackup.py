7c2ff1495166f78057c6ee53a191181d9bcdc4f0
webmaster-exit-1 <mrgofuckyourself@pm.me>
2023-09-20 04:00:20 -0700

    Update test_apkbackup.py

--- tests/test_apkbackup.py
+++ tests/test_apkbackup.py
@@ -4,22 +4,6 @@ import unittest
 from unittest.mock import patch
 import sys
 import os
-import subprocess
-
-
-def get_installed_packages():
-    try:
-        result = subprocess.check_output(['adb', 'shell', 'pm', 'list', 'packages'], text=True)
-        packages = [line.split(":")[1].strip() for line in result.splitlines()]
-        return packages
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing adb command: {e}")
-        return []
-
-# Example usage
-packages = get_installed_packages()
-print(packages)
-
 
 # Adjust the sys.path to include the root directory of your project.
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -29,14 +13,16 @@ from apkbackup import get_installed_packages, backup_apk
 class TestAPKBackup(unittest.TestCase):
 
     @patch('subprocess.run')
-    def test_some_functionality(self, mock_run):
-        mock_run.return_value = "Expected adb output"
+    def test_backup_apk(self, mock_run):
+        mock_package_name = "com.example.app"
+        mock_run.return_value = subprocess.CompletedProcess(args=[], returncode=0, stdout="Expected adb output", stderr="")
 
         # Call your function that interacts with ADB.
-        result = backup_apk("com.example.app")
+        backup_apk('mock_package_name')
 
         # Assert the expected behavior/output.
-        self.assertEqual(result, "Expected output based on mock")
+        # For example, check if the mock_run was called with the expected arguments.
+        mock_run.assert_called_with(['adb', 'shell', 'pm', 'path', mock_package_name], capture_output=True, text=True)
 
 if __name__ == "__main__":
     unittest.main()
